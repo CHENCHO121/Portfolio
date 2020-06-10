0e91772c933a887ddfd400d9bbe79a151fb3fa49
Chencho Gyeltshen <chenchogyeltshen000@gmail.com>
2020-06-10 18:14:23 +0530

    tweat app 4 heroku

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Portfolio.urls'
@@ -124,4 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
+STATICFILES_STORAGE='whitenoise.storage.Compressed.ManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 
